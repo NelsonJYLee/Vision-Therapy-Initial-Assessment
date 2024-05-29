11f97ffa15531b780af50616b14b1219fa63623e
NelsonLee2000 <nelsonleex@gmail.com>
2024-05-28 21:41:00 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+def find_differences(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+
+    i, j = m, n
+    differences = []
+
+    edited = str1
+    while i > 0 and j > 0:
+        if str1[i - 1] == str2[j - 1]:
+            i -= 1
+            j -= 1
+        else:
+            if dp[i][j] == dp[i - 1][j - 1] + 1:
+                differences.append(f"'{str1[i - 1]}' was substituted for '{str2[j - 1]}' at position {i-1}")
+                edited = edited[:i-1] + f"({edited[i-1]})" + edited[i:]
+                i -= 1
+                j -= 1
+            elif dp[i][j] == dp[i - 1][j] + 1:
+                differences.append(f"'{str1[i - 1]}' was added at position {i-1}")
+                edited = edited[:i-1] + f"[{edited[i-1]}]"  + edited[i:]
+                i -= 1
+            elif dp[i][j] == dp[i][j - 1] + 1:
+                differences.append(f"'{str2[j - 1]}' is missing at position {i}")
+                edited = edited[:i] + "_" + edited[i:]
+                j -= 1
+
+    while i > 0:
+        differences.append(f"Delete '{str1[i - 1]}' from str1 at position {i-1}")
+        i -= 1
+
+    while j > 0:
+        differences.append(f"'{str2[j - 1]}' is missing at position {j-1}")
+        edited = edited[:j-1] + "_" + edited[j-1:]
+        j -= 1
+    print(edited, str2)
+    return differences[::-1]
+
+# Example usage:
+str1 = "23467890"
+str2 = "123456789"
+differences = find_differences(str1, str2)
+for diff in differences:
+    print(diff)
