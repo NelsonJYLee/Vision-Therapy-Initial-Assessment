b5a0f98c571121f38763636fd7c0d64889e46c28
NelsonLee2000 <nelsonleex@gmail.com>
2024-06-02 14:53:18 -0700

    added error counters

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#patient sequence:
+str1 = "125341679"
+
+#displayed sequence
+str2 = "123456789"
+
+
+
 def find_differences(str1, str2):
+    sub_and_trans_err = 0
+    omi_err = 0
+    add_err = 0
+
     m, n = len(str1), len(str2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
@@ -25,15 +37,18 @@ def find_differences(str1, str2):
             if dp[i][j] == dp[i - 1][j - 1] + 1:
                 differences.append(f"'{str1[i - 1]}' was substituted for '{str2[j - 1]}' at position {i-1}")
                 edited = edited[:i-1] + f"({edited[i-1]})" + edited[i:]
+                sub_and_trans_err += 1
                 i -= 1
                 j -= 1
             elif dp[i][j] == dp[i - 1][j] + 1:
                 differences.append(f"'{str1[i - 1]}' was added at position {i-1}")
                 edited = edited[:i-1] + f"[{edited[i-1]}]"  + edited[i:]
+                add_err += 1
                 i -= 1
             elif dp[i][j] == dp[i][j - 1] + 1:
                 differences.append(f"'{str2[j - 1]}' is missing at position {i}")
                 edited = edited[:i] + "_" + edited[i:]
+                omi_err += 1
                 j -= 1
 
     while i > 0:
@@ -45,11 +60,13 @@ def find_differences(str1, str2):
         edited = edited[:j-1] + "_" + edited[j-1:]
         j -= 1
     print(edited, str2)
+    print(f"Addition Errors: {add_err}\n"
+      f"Omission Errors: {omi_err}\n"
+      f"Transposition and Substitution Errors: {sub_and_trans_err}")
     return differences[::-1]
 
-# Example usage:
-str1 = "23467890"
-str2 = "123456789"
+
 differences = find_differences(str1, str2)
 for diff in differences:
     print(diff)
+
