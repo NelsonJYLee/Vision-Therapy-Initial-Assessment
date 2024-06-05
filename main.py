479645bafb8df7b78c39913e4c01e13032aeb0c8
NelsonLee2000 <nelsonleex@gmail.com>
2024-06-04 17:13:12 -0700

    switched back to recognizing speech from microphone, implemented continuous recognition, and manual stoppage. transcriber.py is now called in main.py to receive the results from the transcriber. Main.py will now return duration, cleaned patient_numbers, marked_numbers, error counts, and error descriptions

--- main.py
+++ main.py
@@ -1,76 +1,113 @@
-#patient sequence:
-str1 = "12309182"
+from transcriber import recognize_from_microphone
 
-#displayed sequence
-str2 = "123456789"
+transcriber_result = recognize_from_microphone() #{"text":recognized_text, "duration": duration}
 
+#unprocessed string, may contain words or punctuation
+raw_string = transcriber_result["text"]
 
+#duration with many decimal points
+duration = transcriber_result["duration"]
 
-def find_differences(str1, str2):
+#duration rounded to the nearest whole number
+rounded_duration = round(duration)
+
+#processed string, numbers only:
+patient_numbers = ""
+
+#answer key to compare patient_numbers with
+answer_key = "3475529187255377446817447665327992339624"
+
+print(f"Duration: {rounded_duration}s")
+
+#producing patient numbers to only have the numbers from raw_string
+for char in raw_string:
+    if char.isdigit():
+        patient_numbers += char
+print(patient_numbers)
+
+#compares patient_numbers to answer_key, creates a marked_string to show differences in patient_numbers, counts errors, and details errors.
+def find_differences(patient_numbers, answer_key):
+
+    #error tally
     sub_and_trans_err = 0
     omi_err = 0
     add_err = 0
 
-    m, n = len(str1), len(str2)
+    #2D DP - Levenstein Distance
+    m, n = len(patient_numbers), len(answer_key)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
+    #creates a 2D graph for edit distances
     for i in range(m + 1):
         for j in range(n + 1):
             if i == 0:
                 dp[i][j] = j
             elif j == 0:
                 dp[i][j] = i
-            elif str1[i - 1] == str2[j - 1]:
+            elif patient_numbers[i - 1] == answer_key[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
             else:
                 dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
 
     i, j = m, n
+
+    #intialize array to hold strings for error explanation
     differences = []
 
-    edited = str1
+    #initialize marked_numbers variable
+    marked_numbers = patient_numbers
+
+    #going from bottom-right to top-left of graph to gather error counts, error descriptions, and create marked_numbers
     while i > 0 and j > 0:
-        if str1[i - 1] == str2[j - 1]:
+        if patient_numbers[i - 1] == answer_key[j - 1]:
             i -= 1
             j -= 1
         else:
             if dp[i][j] == dp[i - 1][j - 1] + 1:
-                differences.append(f"'{str1[i - 1]}' was substituted for '{str2[j - 1]}' at position {i-1}")
-                edited = edited[:i-1] + f"({edited[i-1]})" + edited[i:]
+                differences.append(f"'{patient_numbers[i - 1]}' was substituted for '{answer_key[j - 1]}' at position {i-1}")
+                marked_numbers = marked_numbers[:i-1] + f"({marked_numbers[i-1]})" + marked_numbers[i:]
                 sub_and_trans_err += 1
                 i -= 1
                 j -= 1
             elif dp[i][j] == dp[i - 1][j] + 1:
-                differences.append(f"'{str1[i - 1]}' was added at position {i-1}")
-                edited = edited[:i-1] + f"[{edited[i-1]}]"  + edited[i:]
+                differences.append(f"'{patient_numbers[i - 1]}' was added at position {i-1}")
+                marked_numbers = marked_numbers[:i-1] + f"[{marked_numbers[i-1]}]"  + marked_numbers[i:]
                 add_err += 1
                 i -= 1
             elif dp[i][j] == dp[i][j - 1] + 1:
-                differences.append(f"'{str2[j - 1]}' is missing at position {i}")
-                edited = edited[:i] + "_" + edited[i:]
+                differences.append(f"'{answer_key[j - 1]}' is missing at position {i}")
+                marked_numbers = marked_numbers[:i] + "_" + marked_numbers[i:]
                 omi_err += 1
                 j -= 1
 
+    #used when j == 0 and i > 0 
     while i > 0:
-        differences.append(f"'{str1[i - 1]}' was added at position{i-1}")
-        edited = edited[:i-1] + f"[{edited[i-1]}]"  + edited[i:]
+        differences.append(f"'{patient_numbers[i - 1]}' was added at position{i-1}")
+        marked_numbers = marked_numbers[:i-1] + f"[{marked_numbers[i-1]}]"  + marked_numbers[i:]
         add_err += 1
         i -= 1
 
+    #used when i == 0 and j > 0
     while j > 0:
-        differences.append(f"'{str2[j - 1]}' is missing at position {j-1}")
-        edited = edited[:j-1] + "_" + edited[j-1:]
+        differences.append(f"'{answer_key[j - 1]}' is missing at position {j-1}")
+        marked_numbers = marked_numbers[:j-1] + "_" + marked_numbers[j-1:]
         j -= 1
-    print(edited, str2)
+    print(marked_numbers + "\n" + answer_key)
 
+    #error counts
     print(f"Addition Errors: {add_err}\n"
       f"Omission Errors: {omi_err}\n"
       f"Transposition and Substitution Errors: {sub_and_trans_err}")
     
+    #error descriptions
     return differences[::-1]
 
 
-differences = find_differences(str1, str2)
+differences = find_differences(patient_numbers, answer_key)
+
+#printing all error descriptions
 for diff in differences:
     print(diff)
+
+
 
--- transcriber.py
+++ transcriber.py
@@ -1,29 +1,64 @@
 import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
+#function detects speech from the microphone and returns the 1)recognized text and 2)duration of recording
 def recognize_from_microphone():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+
+    #confiuring speech_config parameter with key, region, and language
     speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
-    speech_config.speech_recognition_language="en-US"
+    speech_config.speech_recognition_language = "en-US"
 
+    #configuring audio_config with using the default microphone
     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    
+    #recognized_text and duration will be returned at the end of this function
+    recognized_text = ""
+    duration = 0
 
-    print("Speak into your microphone.")
-    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+    #taking a starting time right before recording starts
+    start_time = time.time()
 
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(speech_recognition_result.text))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
+    #callback function for generating recognized_text and error handling
+    def recognized_cb(evt):
+        nonlocal recognized_text
+        if evt.result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = evt.result.text
+        elif evt.result.reason == speechsdk.ResultReason.NoMatch:
+            print("No speech could be recognized: {}".format(evt.result.no_match_details))
+        elif evt.result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = evt.result.cancellation_details
+            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
 
-recognize_from_microphone()
+    #callback function is called when speech is recognized
+    speech_recognizer.recognized.connect(recognized_cb)
+
+    #microphone is continuously listening for speech
+    print("Speak into the microphone.")
+    speech_recognizer.start_continuous_recognition()
+
+    #function continuously runs until CTRL+C is pressed in terminal, leading to stoppage
+    #upon CTRL+C, duration is calculated, and the continuous recognition is stopped
+    #a 2 second delay is provided before the stoppage to allow the recognizer to interpret the last few seconds of speech
+    try:
+        # Keep the script running to listen continuously until interrupted
+        while True:
+            pass
+    except KeyboardInterrupt:
+        end_time = time.time()
+        print("Stopping recognition...")
+        duration = end_time - start_time
+        time.sleep(2)
+        speech_recognizer.stop_continuous_recognition()
+    
+    #dictionary with text and duration keys are returned
+    return {"text":recognized_text, 
+            "duration": duration}
